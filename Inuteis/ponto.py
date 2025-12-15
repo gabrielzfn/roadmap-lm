98eac1aee9b82d525264653b0b91be3ebf58dc8e
FelipeArnt <felipe.arnt@edu.pucrs.br>
2025-12-15 10:00:05 -0300

    Adicionada def (N funciona ainda)

--- Inuteis/ponto.py
+++ Inuteis/ponto.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python3.11
 
+from plyer import notification
+import time
 from datetime import datetime
 
-class Ponto:
-    hi:int
-    mi:int
-    hf:int
-    mf:int
+
+# Função para notificar o horário do
+def notificar_ponto():
+    notification.notify(title=title, message=message, timeout=duration)
+
 
 def marcarPonto():
     # Horas inicio
     hi = int(input("Digite a hora que tu bateu o ponto: "))
     mi = int(input("Digite os minutos: "))
-    
 
     # Horas final
     hf = int(input("Digite a hora final: "))
     mf = int(input("Digite os minutos da hora final: "))
     # Cálculo das horas trabalhadas no dia
-    hora_inicio = datetime(2025,9,16,hi,mi)
-    hora_fim = datetime(2025,9,16,hf,mf)
+    hora_inicio = datetime(2025, 9, 16, hi, mi)
+    hora_fim = datetime(2025, 9, 16, hf, mf)
 
     diferenca = hora_fim - hora_inicio
 
@@ -27,14 +28,14 @@ def marcarPonto():
     horas = int(total_segundos // 3600) - 1
     minutos = int(total_segundos % 3600) // 60
     segundos = int(total_segundos % 60)
-    
+
     print("\f")
     print("\n")
     print(diferenca)
 
     if horas >= 8 and minutos >= 48:
         print("\f" + "Vaza pra casa rapaiz!")
-        print(f"Horas de hoje = {horas}h {minutos}m {segundos}s")        
+        print(f"Horas de hoje = {horas}h {minutos}m {segundos}s")
     else:
         print(f"Horas de hoje = {horas}h {minutos}m {segundos}s")
 
